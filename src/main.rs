a2d8dd4011a90d46ce241c926fed8cf6dcc6a719
Mehak <76558546+Mehak-Mehta@users.noreply.github.com>
2021-06-13 15:01:48 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ mod strs;
 mod types;
 mod tuples;
 mod vectors;
+mod conditionals
 fn main() {
     types :: run();
     strs::run();
     tuples::run();
     vectors::run();
+    conditionals::main();
 }
